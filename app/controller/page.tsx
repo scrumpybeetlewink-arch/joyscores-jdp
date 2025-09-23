35feaebcd4dc900d6aa4aa151505fd3d7c329f1b
Ten <scrumpybeetlewink@gmail.com>
2025-09-23 14:27:59 +0800

    Update page.tsx

--- app/controller/page.tsx
+++ app/controller/page.tsx
@@ -1,10 +1,6 @@
 import dynamic from "next/dynamic";
-
 const ClientController = dynamic(() => import("./ClientController"), {
   ssr: false,
   loading: () => <div className="p-6 text-white">Loading…</div>,
 });
-
-export default function Page() {
-  return <ClientController />;
-}
+export default function Page() { return <ClientController />; }
