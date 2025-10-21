174a9a1671a28f17033f16940f8e5a1d347645b0
Ten <scrumpybeetlewink@gmail.com>
2025-10-21 13:48:31 +0800

    Add files via upload

---
+++ app/controller/court1/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useEffect } from "react";
+
+export default function Page() {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.location.replace("/controller/?court=court1");
+    }
+  }, []);
+
+  return (
+    <div style={ padding: 24 }>
+      Redirecting to <code>/controller/?court=court1</code>…
+    </div>
+  );
+}
