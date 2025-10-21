93ae88d7f668f87bf1e23834b6d62feee05b8376
Ten <scrumpybeetlewink@gmail.com>
2025-10-21 23:28:36 +0800

    Delete app directory

--- app/shared/getCourtId.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-export function getCourtId() {
-  if (typeof window === "undefined") return "court1";
-  const query = new URLSearchParams(window.location.search);
-  const q = query.get("court");
-  return q && /^court[1-5]$/i.test(q) ? q.toLowerCase() : "court1";
-}
