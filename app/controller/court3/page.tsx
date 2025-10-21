e909e5e599db20c00fc674cadaf53de2b2127580
Ten <scrumpybeetlewink@gmail.com>
2025-10-21 13:29:51 +0800

    Delete app/controller/court3 directory

--- app/controller/court3/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-import { useEffect } from "react";
-const TARGET = "/controller/?courtId=court3";
-export default function Page() {
-  useEffect(() => { if (typeof window !== "undefined") window.location.replace(TARGET); }, []);
-  return <div style={{ padding: 24 }}><p>Redirecting to <code>/controller/?courtId=court3</code>…</p></div>;
-}
