daccbe7c15d8c107b04ca9512de5c81877462ed1
Ten <scrumpybeetlewink@gmail.com>
2025-10-23 12:10:53 +0800

    Update page.tsx

--- app/controller1/page.tsx
+++ app/controller1/page.tsx
@@ -2,7 +2,7 @@ import NextDynamic from "next/dynamic";
 
 export const dynamic = "force-static";
 
-const ControllerClient = dynamic(() => import("../_shared/ControllerClient"), { ssr: false });
+const ControllerClient = NextDynamic(() => import("../_shared/ControllerClient"), { ssr: false });
 
 export default function Page() {
   return <ControllerClient courtId="court1" />;
