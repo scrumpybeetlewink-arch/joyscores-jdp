db30c394ff959235e45bc2de669d5a54f18382c5
Ten <scrumpybeetlewink@gmail.com>
2025-10-23 12:19:56 +0800

    Update page.tsx

--- app/live3/page.tsx
+++ app/live3/page.tsx
@@ -1,8 +1,6 @@
-import dynamic from "next/dynamic";
-
 export const dynamic = "force-static";
 
-const LiveClient = dynamic(() => import("../_shared/LiveClient"), { ssr: false });
+import LiveClient from "../_shared/LiveClient";
 
 export default function Page() {
   return <LiveClient courtId="court3" />;
