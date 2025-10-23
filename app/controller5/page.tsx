0e5a1d0c569b0f76649fd08b3691c5478364d26a
Ten <scrumpybeetlewink@gmail.com>
2025-10-23 13:37:38 +0800

    Delete app/controller5 directory

--- app/controller5/page.tsx
+++
@@ -1,7 +0,0 @@
-export const dynamic = "force-static";
-
-import ControllerClient from "../_shared/ControllerClient";
-
-export default function Page() {
-  return <ControllerClient courtId="court5" />;
-}
